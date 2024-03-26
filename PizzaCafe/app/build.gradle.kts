2a22e7a15021013386fb8788227739e57542e7fe
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-03-26 10:56:05 +0300

    dagger dependencies are added

--- PizzaCafe/app/build.gradle.kts
+++ PizzaCafe/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
 }
 
 android {
@@ -51,4 +52,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // dagger
+    implementation("com.google.dagger:dagger:2.51")
+    kapt("com.google.dagger:dagger-compiler:2.51")
 }
