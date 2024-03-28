27216dab7a25264e61d4ff7afcb3453cea806f63
Daniil <109597472+softcatcode@users.noreply.github.com>
2024-03-28 10:15:34 +0300

    dao interface is finished

--- PizzaCafe/app/src/main/java/com/example/pizzacafe/data/FoodDaoInterface.kt
+++ PizzaCafe/app/src/main/java/com/example/pizzacafe/data/FoodDaoInterface.kt
@@ -1,8 +1,29 @@
 package com.example.pizzacafe.data
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.pizzacafe.data.dbModels.CategoryDbModel
+import com.example.pizzacafe.data.dbModels.MenuItemDbModel
 
 @Dao
 interface FoodDaoInterface {
+    @Query("select * from ${MenuItemDbModel.tableName} where categoryName = :categoryName")
+    suspend fun getMenuItems(categoryName: String): List<MenuItemDbModel>
 
+    @Query("select * from ${CategoryDbModel.tableName} where 1")
+    suspend fun getCategories(): List<CategoryDbModel>
+
+    @Query("delete from ${MenuItemDbModel.tableName} where 1")
+    suspend fun clearMenuItems()
+
+    @Query("delete from ${CategoryDbModel.tableName} where 1")
+    suspend fun clearCategories()
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addMenuItem(item: MenuItemDbModel)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addCategory(item: CategoryDbModel)
 }
